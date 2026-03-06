29db9f480cf20f66e549d7975e8f8c8a79f1bbb5
Rostik <rlrosterlkaro@gmail.com>
2026-03-06 14:50:30 +0200

    Update user_1 data with new username and hash

--- Authorization System/dataBase.py
+++ Authorization System/dataBase.py
@@ -1,10 +1,11 @@
 user_1 = {
-    "username": "alex_g",
-    "password_hash": "хешПароля",
+    "username": "alex_h",
+    "password_hash": "hash",
     "first_name": "Алексій",
     "last_name": "Гавриленко",
     "father_name": "Петрович",
     "birth_place": "Івано-Франківськ",
+    "sex": "Ч",
     "family_status": "не одружений",
     "penalty_status": "відсутні"   
 }
