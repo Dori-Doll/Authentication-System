441c64f019c16763198f3843f3cff728c146cc46
Rostik <rlrosterlkaro@gmail.com>
2026-03-06 14:48:27 +0200

    Add files via upload

---
+++ Authorization System/dataBase.py
@@ -0,0 +1,10 @@
+user_1 = {
+    "username": "alex_g",
+    "password_hash": "хешПароля",
+    "first_name": "Алексій",
+    "last_name": "Гавриленко",
+    "father_name": "Петрович",
+    "birth_place": "Івано-Франківськ",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
