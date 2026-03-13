7615b2680ddb88f3d9574fc6dc3b4c372e476601
Rostik <r.chyzhykov@ugi.edu.ua>
2026-03-13 09:33:37 +0200

    Add multiple user entries to the database

--- Authorization System/dataBase.py
+++ Authorization System/dataBase.py
@@ -9,3 +9,122 @@ user_1 = {
     "family_status": "не одружений",
     "penalty_status": "відсутні"   
 }
+
+user_2 = {
+    "username": "soNYA",
+    "password_hash": "hash",
+    "first_name": "Софія",
+    "last_name": "Бондаренко",
+    "father_name": "Іванівна",
+    "birth_place": "Тернопіль",
+    "birth_date": "03.07.02",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "відсутні"   
+}
+
+user_3 = {
+    "username": "Vasyl98",
+    "password_hash": "hash",
+    "first_name": "Василь",
+    "last_name": "Шаповалов",
+    "father_name": "Генадійович",
+    "birth_place": "Дніпро",
+    "birth_date": "21.11.98",
+    "sex": "Ч",
+    "family_status": "одружений",
+    "penalty_status": "1204 грн"   
+}
+
+user_4 = {
+    "username": "MAXIMUS",
+    "password_hash": "hash",
+    "first_name": "Максим",
+    "last_name": "Кочьотов",
+    "father_name": "Матвійович",
+    "birth_place": "Луганськ",
+    "birth_date": "14.05.09",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_5 = {
+    "username": "DmytrOK",
+    "password_hash": "hash",
+    "first_name": "Дмитро",
+    "last_name": "Ковальчук",
+    "father_name": "Олександрович",
+    "birth_place": "Бориспіль",
+    "birth_date": "14.05.09",
+    "sex": "Ч",
+    "family_status": "одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_6 = {
+    "username": "Rosti",
+    "password_hash": "hash",
+    "first_name": "Ростислава",
+    "last_name": "Бикова",
+    "father_name": "Дмитрівна",
+    "birth_place": "Донецьк",
+    "birth_date": "27.03.07",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "27074 грн"   
+}
+
+user_7 = {
+    "username": "Vladdd",
+    "password_hash": "hash",
+    "first_name": "Владислав",
+    "last_name": "Любченко",
+    "father_name": "Костянтинович",
+    "birth_place": "Бердянськ",
+    "birth_date": "20.04.01",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_8 = {
+    "username": "Yarou",
+    "password_hash": "hash",
+    "first_name": "Ярослав",
+    "last_name": "Пилипенко",
+    "father_name": "Костянтинович",
+    "birth_place": "Бердянськ",
+    "birth_date": "02.08.05",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "7658 грн"   
+}
+
+user_9 = {
+    "username": "Olena_P",
+    "password_hash": "hash",
+    "first_name": "Олена",
+    "last_name": "Пилипенко",
+    "father_name": "Костянтинович",
+    "birth_place": "Черкаси",
+    "birth_date": "15.11.96",
+    "sex": "Ж",
+    "family_status": "одружена",
+    "penalty_status": "відсутні"   
+}
+
+user_10 = {
+    "username": "Svit0101",
+    "password_hash": "hash",
+    "first_name": "Світлана",
+    "last_name": "Мельник",
+    "father_name": "Артемівна",
+    "birth_place": "Маріуполь",
+    "birth_date": "09.01.01",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "2078"   
+}
+
+user_list = [user_1, user_2, user_3, user_4, user_5, user_6, user_7, user_8, user_9, user_10]
