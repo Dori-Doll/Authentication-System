5436ff690e8f43600de8e2d4b557ac021bf93855
Rostik <rlrosterlkaro@gmail.com>
2026-03-14 17:39:07 +0200

    Add files via upload

---
+++ Authorization System/Authorization System/function/dataBase.py
@@ -0,0 +1,131 @@
+user_1 = {
+    "username": "alex_h",
+    "password_hash": "$2b$12$IGZYXWQHhiZNvOLlgrqrw.1dcjY2bgRELp22/1oeBWUzvxvDV1VMG",
+    "first_name": "Алексій",
+    "last_name": "Гавриленко",
+    "father_name": "Петрович",
+    "birth_place": "Івано-Франківськ",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_2 = {
+    "username": "soNYA",
+    "password_hash": "$2b$12$4eyeMhft5XeTVhw4kFe7r.Vxo/shDNDXniins3brNP0B.s6d1BzLm",
+    "first_name": "Софія",
+    "last_name": "Бондаренко",
+    "father_name": "Іванівна",
+    "birth_place": "Тернопіль",
+    "birth_date": "03.07.02",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "відсутні"   
+}
+
+user_3 = {
+    "username": "Vasyl98",
+    "password_hash": "$2b$12$Yw37U9dMxskJfwUtrF39cOmYb1ctNTkVJZvBk8UMyfaLR1UPsCF6K",
+    "first_name": "Василь",
+    "last_name": "Шаповалов",
+    "father_name": "Генадійович",
+    "birth_place": "Дніпро",
+    "birth_date": "21.11.98",
+    "sex": "Ч",
+    "family_status": "одружений",
+    "penalty_status": "1204 грн"   
+}
+
+user_4 = {
+    "username": "MAXIMUS",
+    "password_hash": "$2b$12$OiR45Dkcgb5wQIgfNu.q6enMfhSoZ3laDWH1XAQeTxQtYNjC1F8vy",
+    "first_name": "Максим",
+    "last_name": "Кочьотов",
+    "father_name": "Матвійович",
+    "birth_place": "Луганськ",
+    "birth_date": "14.05.09",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_5 = {
+    "username": "DmytrOK",
+    "password_hash": "$2b$12$CjPWGumhAWQ23AdvXktCZO5TUHoRmio8QYckQY.GMg2OQ3StX6Pbu",
+    "first_name": "Дмитро",
+    "last_name": "Ковальчук",
+    "father_name": "Олександрович",
+    "birth_place": "Бориспіль",
+    "birth_date": "14.05.09",
+    "sex": "Ч",
+    "family_status": "одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_6 = {
+    "username": "Rosti",
+    "password_hash": "$2b$12$kATK.EZXyvprxf4aLqOuFuvvgH5lj3fBuQX92dYT.QdDL1Ej0tiZm",
+    "first_name": "Ростислава",
+    "last_name": "Бикова",
+    "father_name": "Дмитрівна",
+    "birth_place": "Донецьк",
+    "birth_date": "27.03.07",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "27074 грн"   
+}
+
+user_7 = {
+    "username": "Vladdd",
+    "password_hash": "$2b$12$rax6YN4oQk4uOY6lgoKQhu/CYroWDmBpH1FZoi25YOEkUmShGmygm",
+    "first_name": "Владислав",
+    "last_name": "Любченко",
+    "father_name": "Костянтинович",
+    "birth_place": "Бердянськ",
+    "birth_date": "20.04.01",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "відсутні"   
+}
+
+user_8 = {
+    "username": "Yarou",
+    "password_hash": "$2b$12$P/ETsBqAaiT7hyVSJ1E1q.IQGmy8mycsbPWIabiopcdh2jXiMMswu",
+    "first_name": "Ярослав",
+    "last_name": "Пилипенко",
+    "father_name": "Костянтинович",
+    "birth_place": "Бердянськ",
+    "birth_date": "02.08.05",
+    "sex": "Ч",
+    "family_status": "не одружений",
+    "penalty_status": "7658 грн"   
+}
+
+user_9 = {
+    "username": "Olena_P",
+    "password_hash": "$2b$12$2ucfSLa8YatkcszZu/HS9.fQDQxeDUYVNmDhvRUF52DmdlBY1FES6",
+    "first_name": "Олена",
+    "last_name": "Пилипенко",
+    "father_name": "Костянтинович",
+    "birth_place": "Черкаси",
+    "birth_date": "15.11.96",
+    "sex": "Ж",
+    "family_status": "одружена",
+    "penalty_status": "відсутні"   
+}
+
+user_10 = {
+    "username": "Svit0101",
+    "password_hash": "$2b$12$qQBQG8b3ADYDHgJZ5CVPMeHiJWrkm3Xr/ztCSEoZxeK5vKtBa15Jy",
+    "first_name": "Світлана",
+    "last_name": "Мельник",
+    "father_name": "Артемівна",
+    "birth_place": "Маріуполь",
+    "birth_date": "09.01.01",
+    "sex": "Ж",
+    "family_status": "не одружена",
+    "penalty_status": "2078"   
+}
+
+user_list = [user_1, user_2, user_3, user_4, user_5, user_6, user_7, user_8, user_9, user_10]
+
